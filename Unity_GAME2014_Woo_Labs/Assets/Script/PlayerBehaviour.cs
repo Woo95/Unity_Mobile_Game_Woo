4a0f5f5ba4ee5119f5c8500df0a94f561aa736c5
5900w <chaewan2002@gmail.com>
2023-09-26 02:59:11 +0900

    modified player behaviour

--- Unity_GAME2014_Woo_Labs/Assets/Script/PlayerBehaviour.cs
+++ Unity_GAME2014_Woo_Labs/Assets/Script/PlayerBehaviour.cs
@@ -7,21 +7,51 @@ public class PlayerBehavior : MonoBehaviour
 	[SerializeField] private Boundaries horizontalBoundaries;
 	[SerializeField] private Boundaries verticalBoundaries;
 
+	Camera cameras;
+	Vector2 destination;
+	[SerializeField] bool isMobilePlatform;
+
 	private Vector2 movement;
 	[SerializeField] private float moveSpeed = 3f;
 	// Start is called before the first frame update
 	void Start()
 	{
-
+		cameras = Camera.main;
+		CheckPlatform();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		if (isMobilePlatform)
+		{
+			TouchScreenInput();
+		}
+		else
+		{
+			PCInput();
+		}
+
+		CheckBoundaries();
+	}
+
+	void TouchScreenInput()
+	{
+		foreach (Touch touch in Input.touches)
+		{
+			destination = cameras.ScreenToWorldPoint(touch.position);
+			transform.position = Vector2.Lerp(transform.position, destination, moveSpeed * Time.deltaTime);
+		}
+	}
+
+	void PCInput()
+	{
 		movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed * Time.deltaTime;
+		transform.position = new Vector2(transform.position.x + movement.x, transform.position.y + movement.y);
+	}
 
-		transform.position = new Vector3(transform.position.x + movement.x, transform.position.y + movement.y, 0);
-
+	void CheckBoundaries()
+	{
 		#region Boundaries
 		if (transform.position.x > horizontalBoundaries.max)
 		{
@@ -40,5 +70,22 @@ public class PlayerBehavior : MonoBehaviour
 			transform.position = new Vector3(transform.position.x, verticalBoundaries.min, transform.position.z);
 		}
 		#endregion
+	}
+
+	void CheckPlatform()
+	{
+		if (Application.platform == RuntimePlatform.Android ||
+			Application.platform == RuntimePlatform.IPhonePlayer)
+		{
+			isMobilePlatform = true;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.CompareTag("Enemy"))
+		{
+			Debug.Log("Got hit");
+		}
 	}
 }
