e42ea5cf98c86c68a6524c03b6a0416ba9eb6a55
5900w <chaewan2002@gmail.com>
2023-09-30 00:36:24 +0900

    added UIPlayScene script

---
+++ Unity_GAME2014_Woo_A1/Assets/Scripts/UIPlayScene.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIPlayScene : MonoBehaviour
+{
+	public GameObject m_GamePlayUI;
+	public GameObject m_GameOverUI;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		if (!m_GamePlayUI.activeInHierarchy)	// make sure gamePlay UI is always on
+			m_GamePlayUI.SetActive(true);
+
+		if (m_GameOverUI.activeInHierarchy)		// make sure gameOver UI is off at the start
+			m_GameOverUI.SetActive(false);
+	}
+
+	public void Invoke_Replay()
+	{
+		SceneManager.LoadScene("PlayScene", LoadSceneMode.Single);
+	}
+
+	public void Invoke_MainMenu_Return()
+	{
+		SceneManager.LoadScene("MenuScene", LoadSceneMode.Single);
+	}
+}
