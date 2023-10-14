a54de2743e4c8bd660567fd247dc558fb1abd893
5900w <chaewan2002@gmail.com>
2023-10-14 16:48:28 +0900

    added buy guardian UI for towers

--- Unity_GAME2014_Woo_A1/Assets/Scripts/2. Others/1. Tower/Tower.cs
+++ Unity_GAME2014_Woo_A1/Assets/Scripts/2. Others/1. Tower/Tower.cs
@@ -5,13 +5,33 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
 	public Guardian[] guardianPrefabs;
+	public Canvas m_PurchaseCanvas;
 
 	public int m_BuyGold;
 	public int m_GoldExp;
 	public int m_Health;
 
+	private void Start()
+	{
+		m_PurchaseCanvas.gameObject.SetActive(false);
+	}
+
 	private void OnMouseDown()
 	{
 		Debug.Log(">>Guardian UI Call");
+		m_PurchaseCanvas.gameObject.SetActive(true);
+	}
+
+	public void Invoke_PurchaseG1()
+	{
+		m_PurchaseCanvas.gameObject.SetActive(false);
+	}
+	public void Invoke_PurchaseG2()
+	{
+		m_PurchaseCanvas.gameObject.SetActive(false);
+	}
+	public void Invoke_PurchaseG3()
+	{
+		m_PurchaseCanvas.gameObject.SetActive(false);
 	}
 }
