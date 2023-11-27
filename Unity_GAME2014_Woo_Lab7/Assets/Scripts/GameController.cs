e9edfa22d597c470c73650db7c6f4a89c686b4ee
5900w <chaewan2002@gmail.com>
2023-11-27 12:20:49 +0900

    added restart feature to work

--- Unity_GAME2014_Woo_Lab7/Assets/Scripts/GameController.cs
+++ Unity_GAME2014_Woo_Lab7/Assets/Scripts/GameController.cs
@@ -1,26 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    GameObject onScreenController;
+	GameObject onScreenController;
     [SerializeField] bool isTesting;
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        if (!isTesting)
-        {
-            onScreenController = GameObject.Find("LeftController");
-            onScreenController.SetActive(Application.platform == RuntimePlatform.WindowsEditor &&
-                                         Application.platform == RuntimePlatform.WindowsPlayer);
-        }       
+        //if (!isTesting)
+        //{
+        //    onScreenController = GameObject.Find("LeftController");
+        //    onScreenController.SetActive(Application.platform == RuntimePlatform.WindowsEditor &&
+        //                                 Application.platform == RuntimePlatform.WindowsPlayer);
+        //}       
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadGameScene()
     {
-        
-    }
+		SceneManager.LoadScene("Game");
+	}
 }
--- Unity_GAME2014_Woo_Lab7/Assets/Scripts/LifeCounter.cs
+++ Unity_GAME2014_Woo_Lab7/Assets/Scripts/LifeCounter.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.SceneManagement;
