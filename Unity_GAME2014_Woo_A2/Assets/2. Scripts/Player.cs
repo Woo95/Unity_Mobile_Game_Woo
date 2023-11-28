ca417c44fb76b22483d048fc86176802b89d0ac9
5900w <chaewan2002@gmail.com>
2023-11-29 06:29:00 +0900

    added more gameplay UIs

--- Unity_GAME2014_Woo_A2/Assets/2. Scripts/Player.cs
+++ Unity_GAME2014_Woo_A2/Assets/2. Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    float m_Speed = 2.5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +14,24 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+		//float hInput = Input.GetAxisRaw("Horizontal");
+
+  //      Vector3 InputVector = new Vector3(hInput,0,0);
+
+		//transform.position += InputVector * m_Speed * Time.deltaTime;
+	}
+
+    public void MoveLeftButton()
+    {
+		transform.position += Vector3.left * m_Speed * Time.deltaTime;
+	}
+    public void MoveRightButton()
+    {
+		transform.position += Vector3.right * m_Speed * Time.deltaTime;
+	}
+    public void JumpButton()
+    {
+
     }
 
     public void OnPlatform(bool OnPlatform, Transform parent = null)
