dddde609a855fcecd4fa56ad8577f2973f785b5d
5900w <chaewan2002@gmail.com>
2023-12-06 02:49:16 +0900

    added enemy scripts

--- Unity_GAME2014_Woo_A2/Assets/2. Scripts/Enemy/Enemy.cs
+++ Unity_GAME2014_Woo_A2/Assets/2. Scripts/Enemy/Enemy.cs
@@ -2,11 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum eEnemyType { Bat, Small_MushRoom, Goblin_Range }
-
-[CreateAssetMenu(fileName = "New Enemy", menuName = "Enemy/New Enemy")]
-public class Enemy : ScriptableObject
+public abstract class Enemy : MonoBehaviour
 {
-	public Sprite m_Sprite;
-	public eEnemyType m_EnemyType;
+	public PlayerController m_Target;
+	public EnemyData m_EnemyData;
+
+	public enum eEnemyState { NONE, IDLE, CHASE, ATTACK };
+
+	public eEnemyState m_EnemyState;
+
+	public abstract void Init();
+
+	#region FSM Idle
+	public abstract void InIdle();
+	public abstract void ModifyIdle();
+	#endregion
+
+	#region FSM CHASE
+	public abstract void InChase();
+	public abstract void ModifyChase();
+	#endregion
+
+	#region FSM ATTACK
+	public abstract void InAttack();
+	public abstract void ModifyAttack();
+	#endregion
+
+	public void Update()
+	{
+		switch (m_EnemyState)
+		{
+			case eEnemyState.IDLE:
+				ModifyIdle(); 
+				break;
+			case eEnemyState.CHASE:
+				ModifyChase();
+				break;
+			case eEnemyState.ATTACK:
+				ModifyAttack();
+				break;
+		}
+	}
 }
---
+++ Unity_GAME2014_Woo_A2/Assets/2. Scripts/Enemy/Monsters/Mushroom.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mushroom : Enemy
+{
+	public override void Init()
+	{
+
+	}
+
+	#region FSM Idle
+	public override void InIdle()
+	{
+
+	}
+	public override void ModifyIdle()
+	{
+
+	}
+	#endregion
+
+	#region FSM CHASE
+	public override void InChase()
+	{
+
+	}
+	public override void ModifyChase()
+	{
+
+	}
+	#endregion
+
+	#region FSM ATTACK
+	public override void InAttack()
+	{
+
+	}
+	public override void ModifyAttack()
+	{
+
+	}
+	#endregion
+}
